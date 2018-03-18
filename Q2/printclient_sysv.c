717d3440e02210421eebf50f2f22242aa4bfb5b8
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-03-18 16:15:26 -0500

    Prepare client to add print jobs

--- Q2/printclient_sysv.c
+++ Q2/printclient_sysv.c
@@ -3,21 +3,29 @@
 //
 // Purpose: Add jobs to be "printed" to the print queue
 // Author: Daniel Lovegrove
-// Version: Mar 15/2018
+// Version: Mar 18/2018
 // *****************************************************************************
 
 #include "manager.h"
 #include <semaphore.h>
 #include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+void insertIntoBoundedBuffer(PrintRequest * req);
+
 
 int main(int argc, char *argv[]) {
-    int shmid;
+    int shmid, i, filesize;
     char *shmseg;
+    long pid = getpid();
+    unsigned int randseed = (unsigned int) pid;
+    char filename[FILENAME_SIZE];
+    PrintRequest request;
 
     // Locate segment
     if ((shmid = shmget(KEY, SIZE, 0666)) < 0) {
@@ -31,11 +39,32 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    // Wait for the printer to have signalled
-
     // Add 6 print jobs to the queue
+    for (i = 0; i < NUM_ITERATIONS; i++) {
+        // Filesize between MIN and MAX
+        filesize = (rand_r(&randseed) % (FILE_SIZE_MAX + 1 - FILE_SIZE_MIN)) + FILE_SIZE_MIN;
+
+        // Put the filename in the buffer, then duplicate it to a new string
+        sprintf(filename, "File-%ld-%d", pid, i);
+
+        // Create the request job
+        request.clientID = pid;
+        strcpy(request.filename, filename);
+        request.fileSize = filesize;
+
+        insertIntoBoundedBuffer(&request);
+
+        // Sleep for up to MAX seconds
+        sleep(rand_r(&randseed) % (SLEEP_TIME_MAX + 1));
+    }
 
     // Exit when done
-
     printf("Client exiting.\n");
+
+    exit(0);
+}
+
+// TODO: Complete
+void insertIntoBoundedBuffer(PrintRequest * req) {
+
 }
