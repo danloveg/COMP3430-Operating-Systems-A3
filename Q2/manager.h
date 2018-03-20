fb493ba8a6b1c37a100ba397129c921b36f0764a
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-03-19 19:11:47 -0500

    Fix semaphore bug: Two sems had the same name

--- Q2/manager.h
+++ Q2/manager.h
@@ -11,9 +11,9 @@
 
 #define KEY 7763168
 #define SIZE 512
-#define MUTEX_SEM_NAME "/mutex"
-#define EMPTY_SEM_NAME "/empty"
-#define FULL_SEM_NAME "/empty"
+#define MUTEX_SEM_NAME "/mutex.sem"
+#define EMPTY_SEM_NAME "/empty.sem"
+#define FULL_SEM_NAME "/full.sem"
 #define QUEUE_LEN 4
 #define BYTES_PER_SEC 4000
 #define BUF_SIZE 32
